08a15773fa02ebec4de4b43c68fcf40cf99d8e76
linyxria <linyxria@gmail.com>
2026-04-25 15:37:36 +0800

    feat: remove email verification

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,7 +2,6 @@ import { betterAuth } from 'better-auth'
 import { drizzleAdapter } from 'better-auth/adapters/drizzle'
 
 import { db } from '@/db'
-import { sendVerificationEmail } from '@/lib/auth-email'
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
@@ -10,27 +9,27 @@ export const auth = betterAuth({
   }),
   emailAndPassword: {
     enabled: true,
-    requireEmailVerification: true,
+    // requireEmailVerification: true,
   },
-  emailVerification: {
-    sendOnSignUp: true,
-    sendOnSignIn: true,
-    autoSignInAfterVerification: true,
-    expiresIn: 60 * 60 * 24,
-    sendVerificationEmail: async ({ user, url }) => {
-      await sendVerificationEmail({
-        email: user.email,
-        name: user.name,
-        verifyUrl: url,
-      })
-    },
-  },
+  // emailVerification: {
+  //   sendOnSignUp: true,
+  //   sendOnSignIn: true,
+  //   autoSignInAfterVerification: true,
+  //   expiresIn: 60 * 60 * 24,
+  //   sendVerificationEmail: async ({ user, url }) => {
+  //     await sendVerificationEmail({
+  //       email: user.email,
+  //       name: user.name,
+  //       verifyUrl: url,
+  //     })
+  //   },
+  // },
   socialProviders: {
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      accessType: 'offline',
-      prompt: 'consent',
+      // accessType: 'offline',
+      // prompt: 'select_account consent',
     },
     github: {
       clientId: process.env.GITHUB_CLIENT_ID,
